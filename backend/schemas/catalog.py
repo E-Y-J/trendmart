1da2638f24a909867c98ffb6336eb43362a802eb
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-10-14 13:17:31 -0400

    feat: Added product input schemas

--- backend/schemas/catalog.py
+++ backend/schemas/catalog.py
@@ -28,8 +28,27 @@ class ReviewSchema(BaseSchema):
         model = Review
 
     # validates rating to be between 0 and 5
-
     @validates('rating')
     def validate_rating(self, value):
         if value < 0 or value > 5:
             raise ValidationError('Rating must be between 0 and 5')
+
+# -------Input Schemas------- #
+
+
+class ProductCreateSchema(BaseSchema):
+    class Meta:
+        model = Product
+        exclude = ('id', 'categories', 'reviews')
+
+    category_ids = fields.List(fields.Integer(), load_only=True)
+    price = fields.Float(required=True, validate=validate.Range(min=0))
+    name = fields.String(required=True, validate=validate.Length(min=1))
+    description = fields.String(validate=validate.Length(min=1))
+    sku = fields.String(required=True, validate=validate.Length(min=1))
+    product_img = fields.String(validate=validate.Length(min=1))
+    tags = fields.String(validate=validate.Length(min=1))
+
+
+class ProductUpdateSchema(ProductCreateSchema):
+    pass
