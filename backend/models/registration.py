a630c8183f1f085f66d566a2f5138e3ba05204e2
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-10-14 18:26:45 -0400

    feat: Removed the PK id from the CustomerProfile model and made the user_id the PK and FK

--- backend/models/registration.py
+++ backend/models/registration.py
@@ -16,8 +16,8 @@ class User(db.Model):
 class CustomerProfile(db.Model):
     __tablename__ = 'customer_profiles'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(20))
