e92bdd01c12f640346725ab33829d26a5c71311e
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-10-14 12:57:00 -0400

    feat: Added input Schemas, UserRegistration and CustomerProfileInput schemas

--- backend/schemas/registration.py
+++ backend/schemas/registration.py
@@ -24,3 +24,24 @@ class UserSchema(BaseSchema):
 
     customer_profile = fields.Nested(CustomerProfileSchema, dump_only=True)
     addresses = fields.Nested(AddressSchema, many=True, dump_only=True)
+
+# -------Input Schemas------- #
+
+
+class UserRegistrationSchema(BaseSchema):
+    class Meta:
+        model = User
+        exclude = ('id', 'created_at', 'active', 'role',
+                   'customer profile', 'addresses')
+
+    password = fields.String(load_only=True, required=True)
+    role = fields.String(dump_only=True, default='customer')
+    email = fields.Email(required=True)
+
+
+class CustomerProfileInputSchema(BaseSchema):
+    class Meta:
+        model = CustomerProfile
+        exclude = ('id', 'user_id', 'default_address', 'user', 'addresses')
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
