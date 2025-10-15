4e8221a186ab811ba21a61a460fd73f500d1b1bd
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-10-14 20:54:00 -0400

    feat(recommendations): implement review and recommendation system with models and schemas

--- backend/schemas/recommendation.py
+++ backend/schemas/recommendation.py
@@ -1,12 +1,16 @@
 from extentions import BaseSchema
 from models.recommendation import Recommendation, Reviews
+from marshmallow import fields
 
 
 class RecommendationSchema(BaseSchema):
     class Meta:
         model = Recommendation
+        include_fk = True
 
 
 class ReviewsSchema(BaseSchema):
     class Meta:
         model = Reviews
+        include_fk = True
+    product_id = fields.Int(required=True)
