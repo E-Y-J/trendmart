6722100ac81a900b64a241a688601fa944df7d30
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-10-16 17:09:42 -0400

    feate(auth): Added auth schemas and password validator

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -1,10 +1,20 @@
 from catalog import CatalogSchemas
 from registration import RegistrationSchemas
 from shopping import ShoppingSchemas
+from auth import AuthSchemas
+from marshmallow import validate
+
+PASSWORD_REGEX = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$'
+PASSWORD_VALIDATOR = validate.Regexp(
+    PASSWORD_REGEX,
+    error='Password must include uppercase, lowercase, number, and special character'
+)
 
 
 __all__ = [
     'CatalogSchemas',
     'RegistrationSchemas',
     'ShoppingSchemas',
+    'AuthSchemas',
+    'PASSWORD_VALIDATOR'
 ]
