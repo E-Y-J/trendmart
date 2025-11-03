05ed33de974886d983096af7b68f386633de7737
Slim-Beatnik <totem64@gmail.com>
2025-11-03 10:01:15 -0700

    Update index.html to include font import head script, removed font import from index.css, Add CheckboxToggle, TextInput, and LoginRegister

---
+++ frontend/src/layouts/layoutChildren/input/TextInput.jsx
@@ -0,0 +1,61 @@
+
+
+function TextInput({ children, inputId, title, placeholder, onChange, disabled=false, info=null, password=false }) {
+  return (
+    <div
+      style={{
+        width: '100%'
+      }}
+    >
+      <label
+        name={ inputId }
+        htmlFor={ inputId }
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '.3rem .3rem .3rem 1rem',
+          gap: '.5rem',
+          border: '2px solid #0a1f44',
+          whiteSpace: 'nowrap',
+          color: '#fffffb',
+          backgroundColor: '#0a1f44',
+          borderRadius: '1rem .4rem .4rem 1rem'
+        }}
+      >
+        { title }
+      <input
+        id={ inputId }
+        name={ inputId }
+        type={ password ? 'password' : 'text'}
+        placeholder={ placeholder }
+        disabled={ disabled }
+        onChange={ onChange }
+        autoComplete="on"
+        style={{
+          width: '100%',
+          border: 'none',
+          justifyContent: 'center',
+          boxSizing: 'border-box',
+          fieldSizing: 'content',
+        }}
+        />
+        {children}
+      </label>
+      { info &&
+        <div
+          style={{
+            fontSize: '.8rem',
+            padding: '.1rem 1rem 1rem 1rem',
+            marginBottom: '1rem'
+          }}
+        >
+          <em>
+            { info }
+          </em>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default TextInput;
