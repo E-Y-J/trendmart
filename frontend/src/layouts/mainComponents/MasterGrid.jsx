9b510e362233c4777bd3969ff28156c6a1548e3d
Slim-Beatnik <totem64@gmail.com>
2025-11-22 11:30:48 -0700

    Update mainComponents

--- frontend/src/layouts/mainComponents/MasterGrid.jsx
+++ frontend/src/layouts/mainComponents/MasterGrid.jsx
@@ -3,23 +3,32 @@ import FeaturedProducts from '../layoutChildren/products/FeaturedProducts';
 import RecommendedProducts from '../layoutChildren/products/RecommendedProducts';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useTheme } from '@styles/themeContext';
+import { useTheme } from '@resources/themes/themeContext';
 
 function MasterGrid() {
   const { theme } = useTheme();
 
   return (
-    <Row className="w-100 h-100 d-flex flex-row m-0" style={{ backgroundColor: theme.colors.text, padding: '2.5rem' }}>
+    <Row
+      className="w-100 h-100 d-flex flex-row m-0"
+      style={{ backgroundColor: theme.colors.whiteBg, padding: '2.5rem' }}
+    >
       <Col
         id="leftCol"
         className="flex-column m-0 p-0 d-none d-sm-flex flex-grow-0-ns align-self-center"
-        style={{ maxWidth: '25%', height: '98%', borderRightStyle: 'solid', borderRightWidth: '.13rem', borderRightColor: '#e8e8e8e8' }}
+        style={{
+          maxWidth: '25%',
+          height: '98%',
+          borderRightStyle: 'solid',
+          borderRightWidth: '.13rem',
+          borderRightColor: theme.colors.details,
+        }}
       >
         <ProductCategories
           categories={['Really long category', 'Short Cat...', 3, 4]}
         />
       </Col>
-      
+
       <Col
         id="rightCol"
         className="d-flex flex-column h-100 w-100 ps-5"
@@ -30,7 +39,7 @@ function MasterGrid() {
           className="d-flex flex-row"
           style={{
             height: '55%',
-            borderBottom: '.13rem solid #e8e8e8e8',
+            borderBottom: `.13rem solid ${theme.colors.details}`,
           }}
         >
           <FeaturedProducts />
@@ -77,7 +86,7 @@ export default MasterGrid;
 
 //       {/* RIGHT MAIN CONTENT */}
 //       <Col className="d-flex flex-column px-3 gap-4">
-        
+
 //         {/* FEATURED SECTION */}
 //         <div className="w-100">
 //           <h4 className="mb-3">Featured Galleries</h4>
